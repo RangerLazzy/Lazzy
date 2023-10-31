1ba47aeca730581575e2f9d287623a4d38582e0a
xiaozzy <54697741+xiaozzydev@users.noreply.github.com>
2023-10-31 22:37:44 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+function getRealUserIp() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    
+    return $ipaddress;
+}
+
+$ip = getRealUserIp();
+
+// Tên file để lưu thông tin
+$file = 'ip.txt';
+
+// Ghi thông tin vào file ip.txt
+$data = "IP: $ip\n";
+file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+?>
+<!--<meta http-equiv="refresh" content="0;url=https://cdgdaily.com/">
